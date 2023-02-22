0a541cc5a01e03abf100835d0517d1bcd8f96f47
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-02-22 23:56:37 +0100

    exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,4 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Total = ({ exercises }) => {
-  return <p>total of {exercises.reduce((a, b) => a + b, 0)} exercises</p>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ courses }) => {
-  return (
-    <>
-      {courses.map((course) => {
-        return (
-          <div key={course.id}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total exercises={course.parts.map((part) => part.exercises)} />
-          </div>
-        );
-      })}
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
