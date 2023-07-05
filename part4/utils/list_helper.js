eb86c6436f725b9b67bbbabfd89fd69635328e5d
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-07-05 14:45:10 +0200

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,10 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  return blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
